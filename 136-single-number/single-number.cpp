15244352ed0d1b85602aff7e0ca8836be228657e
Vanshika <vanshikarajput358@gmail.com>
2026-08-05 00:31:56 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (59.29%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int XOR = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            XOR = XOR^nums[i];
+        }
+        return XOR;
+        
+    }
+};
