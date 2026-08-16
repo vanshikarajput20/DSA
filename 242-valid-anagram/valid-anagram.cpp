4c27c0cfa12d2e41fdcf94473cf3bc477d9ea6dc
Vanshika <vanshikarajput358@gmail.com>
2026-08-16 12:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (49.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -6,14 +6,17 @@ public:
             return false;   
         }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int freq[26] ={0};
 
         for(int i = 0; i < s.length(); i++){
-            if(s[i] != t[i]){
-                return false;
-            }
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
+
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0) return false;
+        }
+
         return true;
     }
 };
