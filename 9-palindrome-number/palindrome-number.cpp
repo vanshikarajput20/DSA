1cd0bac7240ee2d60758453303805bddbb84720e
Vanshika <vanshikarajput358@gmail.com>
2026-08-16 23:51:30 +0530

    Time: 1 ms (57.64%) | Memory: 8.7 MB (19.72%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int reverse(int x) {
+        int y = 0;
+        
+        while(x != 0){
+            int num = x % 10;
+            x = x/10;
+
+            if (y > INT_MAX / 10 || (y == INT_MAX / 10 && num > 7)) {
+                return 0;
+            }
+            if (y < INT_MIN / 10 || (y == INT_MIN / 10 && num < -8)) {
+                return 0;
+            }
+
+
+            y = y * 10 + num;
+        }
+
+        return y;
+    }
+
+    bool isPalindrome(int x) {
+        int rev = reverse(x);
+
+        if(x < 0){
+            return false;
+        }
+
+        if(x == rev){
+            return true;
+        }
+        else return false;
+        
+    }
+};
